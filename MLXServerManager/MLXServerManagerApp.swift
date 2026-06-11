083bb4cbff8e9eb6d12734ca2ccd2dfdc651f147
acari <7821226+acari-git@users.noreply.github.com>
2026-06-11 23:24:25 +0900

    Initial Commit

---
+++ MLXServerManager/MLXServerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLXServerManagerApp.swift
+//  MLXServerManager
+//
+//  Created by yoinkun on 2026/06/11.
+//
+
+import SwiftUI
+
+@main
+struct MLXServerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
